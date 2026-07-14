6156ad6a0c7c622927dc08b3cab4a7a9fa4c268c
Jim Casey <jim.casey@gmail.com>
2026-07-14 10:09:32 -0600

    Created XCode project

---
+++ JackdawTests/JackdawTests.swift
@@ -0,0 +1,19 @@
+//
+//  JackdawTests.swift
+//  JackdawTests
+//
+//  Created by Jim Casey on 7/14/26.
+//
+
+import Testing
+@testable import Jackdaw
+
+struct JackdawTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
